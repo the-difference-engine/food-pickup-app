b666259cdf838c5a56451453df7c5024b4f79398
Lianna Newman <liannanewman@gmail.com>
2016-10-20 20:39:16 -0400

    added admin attribute to donor table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014004444) do
+ActiveRecord::Schema.define(version: 20161021003207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161014004444) do
     t.string   "phone_number"
     t.string   "contact_name"
     t.string   "type_of_donor"
+    t.boolean  "admin"
     t.index ["confirmation_token"], name: "index_donors_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_donors_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_donors_on_reset_password_token", unique: true, using: :btree
