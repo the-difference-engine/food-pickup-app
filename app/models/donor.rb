c07b738feb660982d4c4e1fbda6b8810fdebeeb7
Lianna Newman <liannanewman@gmail.com>
2016-10-26 20:47:33 -0400

    added approve status attribute to donor model, added attributes to signup form, added validations to donor model, added permissions parameters

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -4,4 +4,6 @@ class Donor < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :terms, acceptance: true
+  validates :business_name, :address, :phone_number, :contact_name, :type_of_donor, presence: true
+
 end
