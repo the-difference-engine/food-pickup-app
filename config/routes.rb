b48d3e8db2a5119e82e6add8df6f31ad24f7b7e9
Ana Minaya <analizetteminaya@gmail.com>
2016-10-27 20:18:59 -0400

    added route, show & update action to profile controller & created show view page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/new-pickup' => 'food_pickups#new'
   post '/new-pickup' => 'food_pickups#create'
   get '/profiles' => 'profiles#index'
+  get 'profiles/:id' => 'profiles#show'
+  patch 'profiles/:id' => 'profiles#update'
 end
