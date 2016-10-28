ac1d6588cd9799f6c39647c1d67c9d435ee3c3af
Lianna Newman <liannanewman@gmail.com>
2016-10-27 20:54:20 -0400

    updated index page and routes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,9 @@
 class ProfilesController < ApplicationController
   def index
+    @donors = Donor.where(approved: false)
   end
 
   def show
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'donors#index'
   get '/new-pickup' => 'food_pickups#new'
   post '/new-pickup' => 'food_pickups#create'
+  get '/profiles' => 'profiles#index'
 end
