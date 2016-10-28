b18174273cbcb4160c7f248f6537dbf2b7fec072
Ana Minaya <analizetteminaya@gmail.com>
2016-10-27 20:18:59 -0400

    added route, show & update action to profile controller & created show view page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,20 @@ class ProfilesController < ApplicationController
   end
 
   def show
+    @donor = Donor.find_by(id: params[:id])
   end
+
+  def update
+    @donor = Donor.find_by(id: params[:id])
+    @donor.update(
+    contact_name: params[:contact_name],
+    email: params[:email],
+    business_name: params[:business_name],
+    address: params[:address],
+    phone_number: params[:phone_number],
+    type_of_donor: params[:type_of_donor],
+    approved: true
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :donors
   root 'donors#index'
+
+  get 'profiles/:id' => 'profiles#show'
+  patch 'profiles/:id' => 'profiles#update'
 end
