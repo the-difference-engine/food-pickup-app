9c80bbfca3f0bed53ed09f7161beaf18e70500e4
Lianna Newman <liannanewman@gmail.com>
2017-01-09 20:56:07 -0500

    updated admin index, added pickup edit pg for admin to approve, made changes to pickup routes and  pickups controller

--- app/controllers/food_pickups_controller.rb
+++ app/controllers/food_pickups_controller.rb
@@ -6,26 +6,29 @@ before_action :authenticate_donor!
   end
 
   def create
-        @food_pickup = FoodPickup.create(
-          picture: params[:picture],
-          quantity: params[:quantity],
-          description: params[:description],
-          start_time: params[:start_time],
-          end_time: params[:end_time],
-          location: params[:location],
-          reoccurrence: params[:reoccurrence],
-          charge: params[:number],
-          donor_id: current_donor.id
-        )
+    @food_pickup = FoodPickup.create(
+      picture: params[:picture],
+      quantity: params[:quantity],
+      description: params[:description],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      location: params[:location],
+      reoccurrence: params[:reoccurrence],
+      charge: params[:number]
+    )
 
-      if @food_pickup.valid?
-        redirect_to '/'
-        flash[:success] = "The pickup was successfully created"
-      else
-        flash[:danger] = @food_pickup.errors.full_messages
-        render "new"
-      end
-      @food_pickup.check_reoccurring(@food_pickup)
+    if @food_pickup.valid?
+      DonorPickup.create(
+        donor_id: current_donor.id,
+        food_pickup_id: @food_pickup.id
+      )
+      redirect_to '/'
+      flash[:success] = "The pickup was successfully created"
+    else
+      flash[:danger] = @food_pickup.errors.full_messages
+      render "new"
+    end
+    @food_pickup.check_reoccurring(@food_pickup)
   end
 
   def show
