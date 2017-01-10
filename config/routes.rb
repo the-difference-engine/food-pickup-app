9d0d54194a4b9371c4a2aefb35429a658f17b9ad
Lianna Newman <liannanewman@gmail.com>
2017-01-09 20:56:07 -0500

    updated admin index, added pickup edit pg for admin to approve, made changes to pickup routes and  pickups controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
 
   devise_for :donors
   root 'donors#index'
+<<<<<<< bc0319768bc8099254e6d2bea8f11df7589ca525
   resources :food_pickups
+=======
+  get '/new-pickup' => 'food_pickups#new'
+  post '/new-pickup' => 'food_pickups#create'
+  get '/pickup/:id' => 'food_pickups#show'
+  get '/pickup/:id/edit' => 'food_pickups#edit'
+  patch '/pickup/:id/edit' => 'food_pickups#update'
+>>>>>>> updated admin index, added pickup edit pg for admin to approve, made changes to pickup routes and  pickups controller
   get '/admin' => 'donors#admin_index'
   get 'profiles' => 'profiles#index'
   get 'profiles/:id' => 'profiles#show'
