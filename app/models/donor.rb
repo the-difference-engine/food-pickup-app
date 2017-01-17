8767dffd621eeea142c54ae5c069067162c0393e
Justin Hernandez <jhernandezdev13@gmail.com>
2017-01-16 19:08:56 -0500

    Donor charge now displays on their profile based on type of business and the checkbox on their profile edit toggles the negotiable attribute

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -8,4 +8,19 @@ class Donor < ApplicationRecord
   has_many :food_pickups
   scope :not_admin, -> { where(admin: false) }
 
+  after_create :set_charge_amount
+
+  private
+
+  def set_charge_amount
+    if type_of_donor == 'Office'
+      self.charge = 20.00
+    elsif type_of_donor == 'Restaurant'
+      self.charge = 10.00
+    else
+      self.charge = 100.00
+    end
+      self.save
+  end
+
 end
