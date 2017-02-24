91b0e8987ab31bbb00eb0b7aa2269ea259eb9e49
Justin Hernandez <jhernandezdev13@gmail.com>
2017-02-23 20:06:18 -0500

    custom js form now creates token

---
+++ app/assets/javascripts/billings.js
@@ -0,0 +1,20 @@
+$(function() {
+  var $form = $('#payment-form');
+  $form.submit(function(event) {
+    $form.find('.submit').prop('disabled', true);
+    Stripe.card.createToken($form, stripeResponseHandler);
+    return false;
+  });
+});
+
+function stripeResponseHandler(status, response) {
+  var $form = $('#payment-form');
+  if (response.error) {
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('.submit').prop('disabled', false);
+  } else {
+    var token = response.id;
+    $form.append($('<input type="hidden" name="stripeToken">').val(token));
+    $form.get(0).submit();
+  }
+}
