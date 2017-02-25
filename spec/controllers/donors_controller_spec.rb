05099119f47af42822c5ab2893d0f9884213eff9
Kairav Parekh <kparekh01@gmail.com>
2017-02-25 18:52:44 -0500

    set up testing environment via rspec, factory girl and cleaned up the theme

--- spec/controllers/donors_controller_spec.rb
+++ spec/controllers/donors_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe DonorsController, type: :controller do
-
 end
---
+++ spec/factories/food_pickups.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :food_pickup do
+    #donor_id { FactoryGirl.create(:donor).id }
+    quantity { rand(100) }
+    description { Faker::Lorem.sentence }
+    start_time { 1.hours.from_now }
+    end_time { 2.hours.from_now }
+    location { Faker::Address.street_address }
+    reoccurrence { ["None", "Daily", "Weekly", "Monthly", "Yearly"].sample }
+    charge 20
+  end
+end
