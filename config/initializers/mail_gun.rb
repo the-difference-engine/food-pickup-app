09bb76e32427f6cc60692ae725e6df8dca9d394e
Justin Hernandez <jhernandezdev13@gmail.com>
2017-02-27 18:32:08 -0500

    reconfigured env vars

--- config/initializers/mail_gun.rb
+++ config/initializers/mail_gun.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.mailgun.org",
   :port                   => 587,
   :authentication         => :plain,
-  :user_name =>  ENV['MAILGUN_USERNAME'],
-  :password =>   ENV['MAILGUN_PASSWORD'],
+  :user_name =>  ENV['MAILGUN_SMTP_LOGIN'],
+  :password =>   ENV['MAILGUN_SMTP_PASSWORD'],
   :domain                 => 'rlc-qa.herokuapp.com',
   :enable_starttls_auto   => true
 }
